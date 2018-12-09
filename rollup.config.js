e257d56cf97e75b3fbc3461951e8ecef49be421f
Napat <elecwebmaker@gmail.com>
2018-12-10 04:36:19 +0700

    remove unused code

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
 
 export default {
   input: "src/index.js",
